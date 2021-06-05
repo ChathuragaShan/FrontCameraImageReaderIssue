5a0f66b29ceea5823ee7d45cc0498b456331261b
ChathurangaShan <chathurangashan@gmail.com>
2021-06-05 20:13:05 +0530

    Added dashed circle around the rounded conner selfie cutout custom view.

--- app/src/main/java/com/chathuranga/shan/imagereaderissue/utilities/SelfieCutoutView.kt
+++ app/src/main/java/com/chathuranga/shan/imagereaderissue/utilities/SelfieCutoutView.kt
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.chathuranga.shan.imagereaderissue.R
 
+
 /**
  * This class is responsible for creating view which has a transparent inner circle area
  * which use to show camera preview through
@@ -27,6 +28,9 @@ class SelfieCutoutView(context: Context, attrs: AttributeSet) : View(context, at
 
     // camera cutout radius
     private var circleRadius = 0f
+
+    // handler stroke color
+    private var dotedCircleColor = 0
 
     //view out side fill color
     private var outSideBackgroundColor = 0
@@ -66,6 +70,12 @@ class SelfieCutoutView(context: Context, attrs: AttributeSet) : View(context, at
             .getDimension(R.styleable.SelfieCutoutView_backgroundDrawableHeight, 0f)
         drawableMarginTop = attributeSet
             .getDimension(R.styleable.SelfieCutoutView_backgroundDrawableMarginTop,0f)
+
+        dotedCircleColor =  attributeSet
+            .getColor(
+                R.styleable.SelfieCutoutView_dotedCircleColor,
+                ContextCompat.getColor(context, R.color.white)
+            )
 
         val drawableId = attributeSet
             .getResourceId(R.styleable.SelfieCutoutView_backgroundDrawable, 0)
@@ -130,6 +140,19 @@ class SelfieCutoutView(context: Context, attrs: AttributeSet) : View(context, at
             (mainHeight / 2).toFloat(),
             circleRadius,
             circlePaint
+        )
+
+        val handlerPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        handlerPaint.style = Paint.Style.STROKE
+        handlerPaint.color = dotedCircleColor
+        handlerPaint.strokeWidth = 8f
+        handlerPaint.pathEffect = DashPathEffect(floatArrayOf(20f, 20f), 0.5.toFloat())
+
+        canvas?.drawCircle(
+            (mainWidth / 2).toFloat(),
+            (mainHeight / 2).toFloat(),
+            circleRadius + 20,
+            handlerPaint
         )
     }
 
